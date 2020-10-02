1348b7e1e4267aa47696ade4e6d4c839884f3268
LAPTOP-GOF8R333\SAMSUNG <rywls3597@gmail.com>
2020-10-02 23:48:15 +0900

    selenium modlue 추가

--- YouTube Comment Crawling.py
+++ YouTube Comment Crawling.py
@@ -5,6 +5,11 @@ Created on Fri Oct  2 23:44:49 2020
 @author: SAMSUNG
 """
 
-from selenium import webdriver as wd 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup 
 import time
