a44dba2d825ca6a754c0fab1a33027e0c0450eeb
LAPTOP-GOF8R333\SAMSUNG <rywls3597@gmail.com>
2020-10-03 16:52:49 +0900

    database upload 수정

--- Save_Database.py
+++ Save_Database.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 import MySQLdb as mdb
-import sys    
+from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
 
+user_id = 'root'
+user_pass = 'endeoddl2*'
+host = '127.0.0.1'
+database = 'youtube_comments'
+
+class upload_db:
+    def make_db_connect(self, user_id, user_pass, host, database):
+        engine = create_engine("mysql+mysqldb://{}:{}@{}/{}?charset=utf8".format(user_id, user_pass, host, database), encoding='utf-8')
+        conn = engine.connect()
+        return engine, conn
+    
+    def upload_database(self, table, table_name):
+        ###sql 연결
+        engine, conn = self.make_db_connect(user_id, user_pass, host, database)
+        table.to_sql(name=table_name, con=engine, if_exists='append', index=False)
+        conn.close()
+    
+    def make_dataframe(self, query, columns):
+        engine, conn = self.make_db_connect(user_id, user_pass, host, database)
+        qq = pd.read_sql_query(query, conn)
+        data_frame = pd.DataFrame(qq, columns=columns)
+        conn.close()
+        return data_frame
--- YouTube Comment Crawling.py
+++ YouTube Comment Crawling.py
@@ -12,8 +12,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+from Save_Database import *
+import pymysql
+
 import pandas as pd
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 import os
 import time
 import re
@@ -23,6 +26,11 @@ options.add_argument("headless")
 options.add_argument('window-size=1920x1080')
 options.add_argument('__log-level=3')
 dirr = os.path.dirname(__file__)
+
+#database
+uu_db = upload_db()
+
+#re 정규식
 korean = re.compile('[가-힣]')
 
 
@@ -32,11 +40,15 @@ def get_URL(creater, work):
         driver = webdriver.Chrome(os.path.join(dirr, 'chromedriver.exe'))
     elif work == "not want":
         driver = webdriver.Chrome(os.path.join(dirr, 'chromedriver.exe'),chrome_options=options)
-    
-    #크롤링할 유튜버의 계정
     videos_url = 'https://www.youtube.com/{}/videos'.format(creater)
     driver.get(videos_url)
     
+    #youtuber정보
+    youtube_name = driver.find_element(By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/div[2]/div[2]/div/div[1]/div/div[1]/ytd-channel-name/div/div/yt-formatted-string').text
+    try: #구독자 정보 표시 한 경우
+        subscriber = driver.find_element(By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/div[2]/div[2]/div/div[1]/div/div[1]/yt-formatted-string').text.split(' ')[1]
+    except:
+        subscriber = 'None'
     #유투버 동영상 링크 list
     video_url_list=[]
     
@@ -59,25 +71,31 @@ def get_URL(creater, work):
             video_url_list.append(link['href'])
         except:
             continue
+    
+    #database에 youtuber정보 입력
+    video = len(video_url_list)
+    youtuber_col=['youtube_name', 'subscriber', 'video' ]
+    youtuber_data = {'youtube_name':[youtube_name], 'subscriber':[subscriber], 'video':[video]}
+    youtuber_dataframe=pd.DataFrame(youtuber_data, columns=youtuber_col)
+    uu_db.upload_database(youtuber_dataframe,'youtuber')
+
     driver.close()
     return video_url_list
 
 
 ###댓글 크롤링
-def get_comments(file_name, url_list, work):
+def get_comments(url_list, work):
     if work == 'want':
         driver = webdriver.Chrome(os.path.join(dirr, 'chromedriver.exe'))
     elif work == "not want":
         driver = webdriver.Chrome(os.path.join(dirr, 'chromedriver.exe'),chrome_options=options)
-    
-    videos_dic={}
     
     #url_list의 하나하나의 url에 들어감
     for video in url_list:
         video_url = 'https://www.youtube.com{}'.format(video)
         driver.get(video_url) 
         time.sleep(6.0)
-        
+             
         #댓글 창 로딩
         driver.execute_script("window.scrollTo(0, 400);")
         time.sleep(6.0)
@@ -100,27 +118,55 @@ def get_comments(file_name, url_list, work):
         page = driver.page_source
         soup = BeautifulSoup(page, "html.parser")
         title = soup.find('yt-formatted-string',{'class': 'style-scope ytd-video-primary-info-renderer'}).text
-        print('{}하는중'.format(title))
+        
+        #video정보 
+        video_name = title
+        youtube_name = driver.find_element(By.XPATH,'/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[6]/div[3]/ytd-video-secondary-info-renderer/div/div[2]/ytd-video-owner-renderer/div[1]/ytd-channel-name/div/div/yt-formatted-string/a').text
+        view_number = driver.find_element(By.XPATH,'/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/div[1]/div[1]/yt-view-count-renderer/span[1]').text
+        view_num=re.findall("\d+",view_number)[0]
+        update_date = driver.find_element(By.XPATH,'/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/div[1]/div[2]/yt-formatted-string').text.replace(' ','')
+        
+        #database에 video정보 입력
+        video_col=['video_name', 'youtube_name', 'view_num', 'update_date' ]
+        video_data = {'video_name':[video_name], 'youtube_name':[youtube_name], 'view_num':[view_num], 'update_date':[update_date]}
+        video_dataframe=pd.DataFrame(video_data, columns=video_col)
+        print(video_dataframe)
+        uu_db.upload_database(video_dataframe,'videos')
+        
         comments = soup.find_all('yt-formatted-string',{'id':'content-text'})
-        one_video_dic={}
-        print(len(comments))
+        print('스크롤 내려서 얻은 댓글 개수:',len(comments))
+        comments_dic={}
         for comment in range(1,len(comments)+1):
             like = driver.find_element(By.XPATH,'/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[{}]/ytd-comment-renderer/div[1]/div[2]/ytd-comment-action-buttons-renderer/div[1]/span[2]'.format(comment)).text
             try:
                 #좋아요가 1개 이상인 댓글만 저장
                 if int(like) >= 1:
+                    #comment 정보 
                     nickname=driver.find_element(By.XPATH,'/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[{}]/ytd-comment-renderer/div[1]/div[2]/div[1]/div[2]/a/span'.format(comment)).text
                     n_comment=driver.find_element(By.XPATH,'/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[{}]/ytd-comment-renderer/div[1]/div[2]/ytd-expander/div/yt-formatted-string[2]'.format(comment)).text
-                    one_video_dic[nickname]=[n_comment,like]
+                    comments_dic[comment]=[nickname, like, n_comment, video_name]
             except:
                 continue
-        videos_dic[title]=one_video_dic
+            
+        #database에 comment정보 입력
+        comments_dataframe=pd.DataFrame(comments_dic).T
+        comments_dataframe.columns=['commneter_nickname', 'like_num', 'comment_content', 'video_name']
+        print(comments_dataframe)
+        uu_db.upload_database(comments_dataframe,'comments')
     
-    #dataframe으로 만들어서 csv파일로 저장
-    comment_dataframe = pd.DataFrame(videos_dic)
-    comment_dataframe.to_csv('result/{}_result.csv'.format(file_name), encoding='utf-8')
+        #commnets_no 1부터 정렬
+        cpnn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='endeoddl2*', db='youtube_comments')
+        curs = cpnn.cursor()
+        curs.execute('SET @COUNT = 0;')
+        curs.execute('ALTER TABLE comments AUTO_INCREMENT = 1;')
+        curs.execute('UPDATE comments SET commnets_no = @COUNT:=@COUNT+1;')
+        cpnn.commit()
+        cpnn.close()
+
     driver.close()
-    return videos_dic
         
 url_list=get_URL('user/hyeyounga', 'want')
-videos_dic=get_comments('여수언니 정혜영',url_list, 'want')
+get_comments(url_list, 'want')
+
+
+
